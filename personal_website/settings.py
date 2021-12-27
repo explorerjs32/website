1332bf9711026aef33311da548d8c1a317ffd11f
Francisco Mendez <explorerjs32@gmail.com>
2021-12-27 10:30:52 -0500

    first commit

--- personal_website/settings.py
+++ personal_website/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = 'django-insecure-ipbc@s6sbf)f6u59(-h#i&g!rnfg8$m3k*#7x_c2aw#!5f4-&0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
